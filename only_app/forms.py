36d80ac43b0995037b50779dc0b3f346a580b660
SebinDuke <sebinduke@localhost.localdomain>
2016-11-26 15:34:56 -0500

    Sebin

--- only_app/forms.py
+++ only_app/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class GitLink(forms.Form):
     link=forms.CharField(max_length = 100)
     pname= forms.CharField(max_length=80)
+
+class IP(forms.Forms):
+    input=forms.CharField(max_length=200)
--- only_app/views.py
+++ only_app/views.py
@@ -4,12 +4,20 @@ import os
 import subprocess
 
 from .api_do import my_server
-from only_app.forms import GitLink
+from only_app.forms import GitLink,IP
 
 def index(request):
     return render(request, 'Temp/main.html')
 
 def openTer(request):
+    if request.method == 'POST':
+        ip=IP(request.POST)
+        if ip.is_valid():
+            input=ip.cleaned_data.get('input')
+            inp=input.split()
+            subprocess.call(inp)
+        else:
+            return HttpResponse("Form not valid")
     return render(request, 'Temp/term.html')
 
 
@@ -23,6 +31,8 @@ def getTer(request):
         if gitlink.is_valid():
             link = gitlink.cleaned_data.get('link')
             pname= gitlink.cleaned_data.get('pname')
+        else:
+            return HttpResponse("Form not valid")
     subprocess.call(["git","clone",link])
     os.chdir(pname)
     subprocess.call(["ls"])
